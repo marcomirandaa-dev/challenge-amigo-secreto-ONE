c1da3a38183d6ef87c0397e39215dbd67392786d
Marco Miranda <marcomirandaa.dev@gmail.com>
2025-08-05 22:42:51 -0300

    Implementa função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -8,4 +8,17 @@ function adicionarAmigo(){
         ListaDeAmigos.push(amigo);
         document.querySelector('input').value = '';
     }
+    atualizarListaDeAmigos()
+}
+
+function atualizarListaDeAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (i = 0; i<ListaDeAmigos.length; i++){
+
+        let li = document.createElement('li');
+        li.textContent = ListaDeAmigos[i];
+        lista.appendChild(li);
+    }
 }
